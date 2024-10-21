bd2952e2476097e921e264ddb36e2b488f9d6c59
Andrea Ferrer Hamilton <112133010+andfhamilton@users.noreply.github.com>
2024-10-21 11:57:49 -0700

    update 1

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.FloatField(default=0)
+    unit = models.CharField(max_length=200)
+    unit_price = models.FloatField(default=0)
+
+    def __str__(self):
+        return f""" name={self.name}; qty={self.quantity}; unit={self.unit}; unit_price={self.unit_price}"""
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField(default=0)
+
+    def __str__(self):
+        return f""" title={self.title}; price={self.price} """
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
+
+    def __str__(self):
+        return f""" menu_item=[{self.menu_item.__str__()}]; ingredient={self.ingredient.name}; qty={self.quantity} """
+    
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f""" menu_item=[{self.menu_item.__str__()}]; time={self.timestamp} """
